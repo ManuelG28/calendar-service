e30d936d85873d19c6f0747406be3d0d633b8591
Manuel Gutierrez <manuelgutierrez@Manuels-MacBook-Pro.local>
2022-07-21 14:39:13 -0500

    Initialize project

---
+++ src/test/java/com/idea/calendarservice/CalendarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idea.calendarservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
