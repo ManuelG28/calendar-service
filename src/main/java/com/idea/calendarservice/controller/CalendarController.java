aaa7de23911f92a2532a910db4b4aca108821127
Manuel Gutierrez <manuelgutierrez@Manuels-MacBook-Pro.local>
2022-08-19 10:05:04 -0500

    Improve documentation on methods

--- src/main/java/com/idea/calendarservice/controller/CalendarController.java
+++ src/main/java/com/idea/calendarservice/controller/CalendarController.java
@@ -5,8 +5,14 @@ import static com.idea.calendarservice.controller.Endpoints.CALENDARS;
 import static com.idea.calendarservice.controller.Endpoints.V_1;
 
 import com.idea.calendarservice.db.CalendarEntity;
+import com.idea.calendarservice.model.ApiErrorResponse;
 import com.idea.calendarservice.model.Calendar;
 import com.idea.calendarservice.service.CalendarService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.net.URI;
 import java.util.List;
 import javax.validation.Valid;
@@ -30,6 +36,13 @@ public class CalendarController {
 
   private final CalendarService calendarService;
 
+  @Operation(summary = "Create a calendar")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "201", description = "Calendar created successfully"),
+      @ApiResponse(responseCode = "400", description = "Invalid calendar object supplied", content = {
+          @Content(mediaType = "application/json",
+              schema = @Schema(implementation = ApiErrorResponse.class))})
+  })
   @PostMapping
   public ResponseEntity<Void> addCalendar(@Valid @RequestBody Calendar calendar,
       UriComponentsBuilder uriComponentsBuilder) {
@@ -39,27 +52,49 @@ public class CalendarController {
     return ResponseEntity.created(uri).build();
   }
 
-  @GetMapping
-  public ResponseEntity<List<Calendar>> getAllCalendars() {
-    return ResponseEntity.ok(calendarService.getAllCalendars());
-  }
-
+  @Operation(summary = "Get calendar paginated")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Calendars retrieve successfully"),
+  })
   @GetMapping(params = {"page"})
   public ResponseEntity<List<Calendar>> getCalendarsPaginated(@RequestParam("page") int page) {
     return ResponseEntity.ok(calendarService.getCalendarsPaginated(page));
   }
 
+  @GetMapping
+  public ResponseEntity<List<Calendar>> getAllCalendars() {
+    return ResponseEntity.ok(calendarService.getAllCalendars());
+  }
+
+  @Operation(summary = "Get calendar by its date")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Calendars retrieve successfully"),
+  })
   @GetMapping("/date/{date}")
   public ResponseEntity<List<Calendar>> getCalendarByDate(@PathVariable String date) {
     return ResponseEntity.ok(calendarService.getCalendarsByDate(date));
   }
 
+  @Operation(summary = "Update a calendar")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "204", description = "Calendar updated successfully"),
+      @ApiResponse(responseCode = "400", description = "Invalid calendar object supplied", content = {
+          @Content(mediaType = "application/json",
+              schema = @Schema(implementation = ApiErrorResponse.class))})
+  })
   @PutMapping
   public ResponseEntity<Void> updateCalendar(@Valid @RequestBody Calendar calendar) {
     calendarService.updateCalendar(calendar);
     return ResponseEntity.noContent().build();
   }
 
+  @Operation(summary = "Delete a calendar by its id")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "204", description = "Calendar deleted successfully"),
+      @ApiResponse(responseCode = "404", description = "Calendar not found", content = {
+          @Content(mediaType = "application/json",
+              schema = @Schema(implementation = ApiErrorResponse.class))})
+  })
   @DeleteMapping("/{id}")
   public ResponseEntity<Void> deleteCalendarById(@PathVariable Long id) {
     calendarService.deleteCalendarById(id);
